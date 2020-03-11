90dcda184f08a2927c0e197a833e5d822ff1c2cb
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-11 21:44:10 +0000

    added timeout to client.js

--- client.js
+++ client.js
@@ -1,13 +1,35 @@
 const net = require('net');
+
+
 const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.88.177',
     port: 50541
   });
 
+  const newFrame = 50;
+
   conn.on('connect', () => {
     conn.write('Name: JAS');
   });
+ 
+  let up = conn.on('connect', () => {
+    //setTimeout(() => conn.write('Move: up'), newFrame);
+  });
+
+  let down = conn.on('connect', () => {
+    setTimeout(() => conn.write('Move: down'), newFrame);
+  });
+ 
+  let left = conn.on('connect', () => {
+    setTimeout(() => conn.write('Move: left'), newFrame);
+  });
+ 
+  let  right = conn.on('connect', () => {
+    setTimeout(() => conn.write('Move: right'), newFrame);
+  });
+  
+  
 
   conn.setEncoding('utf8'); 
 
